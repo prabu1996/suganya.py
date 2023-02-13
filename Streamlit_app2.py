7937c74fdfc714513f13329702c78598003b4cbd
prabu1996 <93570188+prabu1996@users.noreply.github.com>
2023-02-13 17:11:28 +0530

    Create Streamlit_app2.py

---
+++ Streamlit_app2.py
@@ -0,0 +1,12 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 and blueberry oatmeal')
+streamlit.text('kale ,spinach & rocket smoothie')
+streamlit.text('hard-boiled egg')
+streamlit.text('chocolates')
+streamlit.header('🍇build your 🍌🥭first smoothie🍇')
